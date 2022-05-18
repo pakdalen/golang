87e35173cc895f1af8c63be08c4958367341fded
pakdalen <47467228+pakdalen@users.noreply.github.com>
2022-05-18 08:09:25 +0600

    Add files via upload

--- endProject/cmd/server/main.go
+++ endProject/cmd/server/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"endProject/pkg/api"
 	"endProject/pkg/students"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/test"
 	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
+	go func() {
+		//mux
+		mux := runtime.NewServeMux()
+		pb.RegisterTestApiHandlerServer(context.Background(), mux, &test.ApiServer{})
+		log.Fatal(http.ListenAndServe("localhost:8081", mux))
+	}()
 	a := grpc.NewServer()
 	server := &students.GRPCServer{}
 	api.RegisterStudentsServer(a, server)
@@ -19,4 +30,10 @@ func main() {
 	if err := a.Serve(j); err != nil {
 		log.Fatal(err)
 	}
+	connStr := "user= postgres password=postgres dbname= postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
